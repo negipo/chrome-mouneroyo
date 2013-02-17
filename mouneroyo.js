1088f667ea415888ce44cf13c71026d2a689aa18
negipo <negipo@gmail.com>
2013-02-17 18:00:40 +0900

    Add title change

--- mouneroyo.js
+++ mouneroyo.js
@@ -1,6 +1,7 @@
 var nowHour = (new Date()).getHours();
 if (0 <= nowHour && nowHour <= 7) {
   window.addEventListener('load', function() {
+    document.getElementsByTagName('title')[0].innerHTML = 'もう寝ろよ';
     document.getElementById('mouneroyo').style.display = 'block';
     document.getElementById('audio').play();
   }, false);
