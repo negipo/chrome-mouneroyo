f836364bec9bb8f82f0a433b7392d07d3ee697f3
negipo <negipo@gmail.com>
2013-03-13 22:20:59 +0900

    Add madanenakuteii message by default

--- mouneroyo.js
+++ mouneroyo.js
@@ -1,8 +1,10 @@
 var nowHour = (new Date()).getHours();
-if (0 <= nowHour && nowHour <= 7) {
-  window.addEventListener('load', function() {
+window.addEventListener('load', function() {
+  if (0 <= nowHour && nowHour <= 7) {
     document.getElementsByTagName('title')[0].innerHTML = 'もう寝ろよ';
     document.getElementById('mouneroyo').style.display = 'block';
     document.getElementById('audio').play();
-  }, false);
-}
+  } else {
+    document.getElementById('madanenakuteii').style.display = 'block';
+  }
+}, false);
