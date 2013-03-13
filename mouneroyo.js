0617e436dfe6157a02f5bf84cdf906b0e69c0b09
negipo <negipo@gmail.com>
2013-03-13 22:21:27 +0900

    Define after 7am as morning

--- mouneroyo.js
+++ mouneroyo.js
@@ -1,6 +1,6 @@
 var nowHour = (new Date()).getHours();
 window.addEventListener('load', function() {
-  if (0 <= nowHour && nowHour <= 7) {
+  if (0 <= nowHour && nowHour <= 6) {
     document.getElementsByTagName('title')[0].innerHTML = 'もう寝ろよ';
     document.getElementById('mouneroyo').style.display = 'block';
     document.getElementById('audio').play();
